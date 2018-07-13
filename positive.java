da78ab96055547d2ce2fa491c856041dd9790829
sellakkannuS <41118101+sellakkannuS@users.noreply.github.com>
2018-07-13 09:54:45 +0530

    Add files via upload

---
+++ positive.java
@@ -0,0 +1,20 @@
+package sellam;
+import java.util.Scanner;
+public class positive {
+	public static void main(String[] args){
+		int a;
+		System.out.println("enter the number");
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		if(a==0){
+			System.out.println("zero");
+		}
+		else if(a>=0){
+			System.out.println("positive");
+		}
+		System.out.println("negative");
+		}
+		}
+	
+
+
